4777f0e6acad8996b8f3295e66967035ba541592
Natiq <natiq00h2272@gmail.com>
2023-03-05 13:51:01 +0400

    UI - HomeScreen.kt architecture and vision styling created and implemented - 3

--- app/src/main/java/com/natiqhaciyef/millisoft_room_tracker/view/screen/home/HomeScreen.kt
+++ app/src/main/java/com/natiqhaciyef/millisoft_room_tracker/view/screen/home/HomeScreen.kt
@@ -1,11 +1,10 @@
 package com.natiqhaciyef.millisoft_room_tracker.view.screen.home
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Scaffold
@@ -15,9 +14,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.natiqhaciyef.millisoft_room_tracker.R
 import com.natiqhaciyef.millisoft_room_tracker.view.navigation.ScreenID
 
 //@Preview
@@ -27,6 +30,7 @@ fun HomeScreen(
 ) {
     HomeTopView {
         HomeBodyView()
+        HomeBottomView()
     }
 }
 
@@ -45,6 +49,7 @@ fun HomeTopView(content: @Composable () -> Unit = { }) {
     }
 }
 
+@Preview
 @Composable
 fun HomeBodyView() {
     Spacer(modifier = Modifier.height(280.dp))
@@ -71,6 +76,93 @@ fun HomeBodyView() {
                     backgroundColor = Color.Gray,
                     shape = RoundedCornerShape(15.dp)
                 ) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.android_development),
+                            contentDescription = "Android Development",
+                            modifier = Modifier
+                                .padding(start = 35.dp, end = 35.dp)
+                        )
+                        Spacer(modifier = Modifier.height(10.dp))
+
+                        Text(
+                            text = "Android Development",
+                            fontSize = 13.sp,
+                            fontWeight = FontWeight.SemiBold,
+                            color = White
+                        )
+                    }
+
+                }
+
+                Spacer(modifier = Modifier.width(20.dp))
+
+                Card(
+                    modifier = Modifier
+                        .size(160.dp),
+                    backgroundColor = Color.Gray,
+                    shape = RoundedCornerShape(15.dp)
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.java_development),
+                            contentDescription = "Java Development",
+                            modifier = Modifier
+                                .padding(start = 35.dp, end = 35.dp)
+                        )
+                        Spacer(modifier = Modifier.height(10.dp))
+
+                        Text(
+                            text = "Java Development",
+                            fontSize = 13.sp,
+                            fontWeight = FontWeight.SemiBold,
+                            color = White
+                        )
+                    }
+
+                }
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Card(
+                    modifier = Modifier
+                        .size(160.dp),
+                    backgroundColor = Color.Gray,
+                    shape = RoundedCornerShape(15.dp)
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.frontend),
+                            contentDescription = "FrontEnd Development",
+                            modifier = Modifier
+                                .padding(start = 35.dp, end = 35.dp)
+                        )
+                        Spacer(modifier = Modifier.height(10.dp))
+
+                        Text(
+                            text = "Front-End Development",
+                            fontSize = 13.sp,
+                            fontWeight = FontWeight.SemiBold,
+                            color = White
+                        )
+                    }
 
                 }
 
@@ -112,35 +204,34 @@ fun HomeBodyView() {
 
                 }
             }
-
-            Spacer(modifier = Modifier.height(20.dp))
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Card(
-                    modifier = Modifier
-                        .size(160.dp),
-                    backgroundColor = Color.Gray,
-                    shape = RoundedCornerShape(15.dp)
-                ) {
-
-                }
-
-                Spacer(modifier = Modifier.width(20.dp))
-
-                Card(
-                    modifier = Modifier
-                        .size(160.dp),
-                    backgroundColor = Color.Gray,
-                    shape = RoundedCornerShape(15.dp)
-                ) {
-
-                }
-            }
-
-            Spacer(modifier = Modifier.height(75.dp))
+            Spacer(modifier = Modifier.height(30.dp))
         }
+    }
+}
+
+
+@OptIn(ExperimentalFoundationApi::class)
+@Preview
+@Composable
+fun HomeBottomView() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            modifier = Modifier.padding(start = 20.dp),
+            text = "Instructors",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Black
+        )
+        Spacer(modifier = Modifier.height(22.dp))
+
+        HorizontalPager(pageCount = 3) {
+
+        }
+        Spacer(modifier = Modifier.height(75.dp))
     }
 }
