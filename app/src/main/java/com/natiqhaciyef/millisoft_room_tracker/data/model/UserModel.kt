cc8ad233d02a32b9982f3365624c2bc782811ad9
Natiq <natiq00h2272@gmail.com>
2023-03-10 18:11:20 +0400

    UI - HomeScreen bottom view update

---
+++ app/src/main/java/com/natiqhaciyef/millisoft_room_tracker/data/model/UserModel.kt
@@ -0,0 +1,7 @@
+package com.natiqhaciyef.millisoft_room_tracker.data.model
+
+data class UserModel(
+    var id: Int,
+    var name: String,
+    var email: String,
+)
--- app/src/main/java/com/natiqhaciyef/millisoft_room_tracker/view/components/Components.kt
+++ app/src/main/java/com/natiqhaciyef/millisoft_room_tracker/view/components/Components.kt
@@ -1,26 +1,44 @@
 package com.natiqhaciyef.millisoft_room_tracker.view.components
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.util.lerp
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.calculateCurrentOffsetForPage
+import com.google.accompanist.pager.rememberPagerState
 import com.natiqhaciyef.millisoft_room_tracker.R
 import com.natiqhaciyef.millisoft_room_tracker.data.model.NavItemModel
+import com.natiqhaciyef.millisoft_room_tracker.data.util.InstructorList
+import com.natiqhaciyef.millisoft_room_tracker.ui.theme.PurpleExtraDark
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.yield
+import kotlin.math.absoluteValue
 
 @Composable
 fun BottomShadow(alpha: Float = 0.1f, height: Dp = 8.dp,
@@ -112,6 +130,101 @@ fun NavBar(
                             )
                         }
                     }
+                }
+            }
+        }
+    }
+}
+
+
+
+@Preview
+@ExperimentalPagerApi
+@Composable
+fun CustomViewPager() {
+    val pagerState = rememberPagerState(
+        initialPage = 0,
+        pageCount = InstructorList.instructors.size
+    )
+
+    LaunchedEffect(key1 = Unit) {
+        while (true) {
+            yield()
+            delay(2000)
+            pagerState.animateScrollToPage(
+                page = (pagerState.currentPage + 1) % (pagerState.pageCount),
+                animationSpec = tween(600)
+            )
+        }
+    }
+
+    HorizontalPager(
+        state = pagerState,
+        modifier = Modifier
+            .padding(horizontal = 40.dp)
+    ) { page ->
+
+        Card(
+            modifier = Modifier
+                .graphicsLayer {
+                    val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
+                    lerp(
+                        start = 0.85f,
+                        stop = 1f,
+                        fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                    ).also { scale ->
+                        scaleX = scale
+                        scaleY = scale
+                    }
+                    alpha = lerp(
+                        start = 0.5f,
+                        stop = 1f,
+                        fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                    )
+                }
+                .fillMaxWidth()
+                .height(295.dp)
+                .padding(horizontal = 40.dp),
+            shape = RoundedCornerShape(15.dp)
+        ) {
+            val item = InstructorList.instructors[page]
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .align(Alignment.Center)
+                    .background(color = PurpleExtraDark)
+            ) {
+                Column(
+                    modifier = Modifier,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Image(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(230.dp),
+                        contentScale = ContentScale.Crop,
+                        painter = painterResource(id = item.image),
+                        contentDescription = "Instructor",
+                    )
+
+                    Spacer(modifier = Modifier.height(5.dp))
+
+                    Text(
+                        text = item.name,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        color = Color.White
+                    )
+
+                    Spacer(modifier = Modifier.height(5.dp))
+
+                    Text(
+                        text = item.field,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        color = Color.White
+                    )
                 }
             }
         }
