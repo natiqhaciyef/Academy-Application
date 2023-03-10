d787c57b308cc245a3eaebb78726372a92d0ac12
Natiq <natiq00h2272@gmail.com>
2023-03-10 09:22:31 +0400

    UI - HomeScreen bottom view (horizontal view pager) created and Instructors view, name and field implemented. Created InstructorModel.kt class

--- app/src/main/java/com/natiqhaciyef/millisoft_room_tracker/data/util/InstructorList.kt
+++ app/src/main/java/com/natiqhaciyef/millisoft_room_tracker/data/util/InstructorList.kt
@@ -1,11 +1,12 @@
 package com.natiqhaciyef.millisoft_room_tracker.data.util
 
 import com.natiqhaciyef.millisoft_room_tracker.R
+import com.natiqhaciyef.millisoft_room_tracker.data.model.InstructorModel
 
 object InstructorList {
-    val instructors = listOf<Int>(
-        R.drawable.natiq,
-        R.drawable.ugur,
-        R.drawable.rufet,
+    val instructors = listOf<InstructorModel>(
+        InstructorModel(name = "Natiq Hacıyev", field = "Mobile Developer",image = R.drawable.natiq),
+        InstructorModel(name = "Uğur Kərimov", field = "Java Backend Developer",image = R.drawable.ugur),
+        InstructorModel(name = "Rüfət Hüseynov", field = "Java Backend Developer",image = R.drawable.rufet),
     )
 }
